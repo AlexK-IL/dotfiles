48f4e872048da584b98d0e9c5102238a5c9dee5b
AlexK-IL <alexkubicail@gmail.com>
2016-01-30 13:34:19 +0200

    modified cmus current playing script

--- i3/cmus_current_song.sh
+++ i3/cmus_current_song.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 # A shell script to print current song being played on cmus
 
+# Check if cmus is installed
 if [ ! -x /usr/bin/cmus-remote ];
 then
-  echo "cmus isn't installed"
+  echo "cmus not installed"
   exit
 fi
 
-STATUS=$( cmus-remote -Q 2>/dev/null | grep status | cut -d " " -f 2- )
-FILE=$( cmus-remote -Q 2>/dev/null | grep file | cut -d " " -f 2- )
+# Get status of cmus
+STATUS=$( cmus-remote -Q 2>/dev/null | grep "status" | cut -d " " -f 2- )
+# Get Artist tag of current song
+ARTIST=$( cmus-remote -Q 2>/dev/null | grep "tag artist" | cut -d " " -f 3- )
+# Get Artist tag of current song
+TITLE=$( cmus-remote -Q 2>/dev/null | grep "tag title" | cut -d " " -f 3- )
 
+# If not artist or title tags available, get filename
+if [ -z "$ARTIST" -o -z "$TITLE" ];
+then
+  # Get full path to file and extract filename
+  FILE=$( cmus-remote -Q 2>/dev/null | grep "file" | cut -d " " -f 2- | rev | cut -d "/" -f1 | rev )
+fi
+
+# Check if cmus is playing
 if [ "$STATUS" == "playing" ];
 then
-  echo "$FILE"
+  # Check if file wasn't set
+  if [ -z "$FILE" ];
+  then
+    # Print artist - title
+    echo "$ARTIST - $TITLE" 
+  else
+    # Print filename
+    echo "$FILE"
+  fi
 else
-  echo "Cmus is off"
+  echo "Not Playing"
 fi
