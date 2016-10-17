36b2e18e3079074b6fda08045f939d238dfed280
Alex Kubica <alexkubicail@gmail.com>
2016-10-17 11:31:27 +0300

    fix i3lock wrapper script

--- home/alex/scripts/i3lockwrapper.sh
+++ home/alex/scripts/i3lockwrapper.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # Shell script to wrap i3lock
 
-# Change layout to english
+# Change layout to english only
 setxkbmap us
-/usr/bin/sleep 1
-setxkbmap -layout us,il -option grp:alt_shift_toggle
 
 # Lock screen
 /usr/bin/i3lock --image /home/alex/Pictures/lockscreen.png --show-failed-attempts -c 000000
+
+# Return layout
+setxkbmap -layout us,il -option grp:alt_shift_toggle
