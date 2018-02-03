c75dc3de3af25615d8ac261d7b213802e0809f06
Alex Kubica <alexkubicail@gmail.com>
2018-02-03 21:25:17 +0200

    modify polybar power menu

--- bin/lockscreen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Make sure to set keyboard layout to english before locking the screen
-setxkbmap us & betterlockscreen --lock & setxkbmap -layout us,il -option grp:alt_shift_toggle
--- bin/suspend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Make sure to set keyboard layout to english before locking the screen
-setxkbmap us & betterlockscreen --suspend & setxkbmap -layout us,il -option grp:alt_shift_toggle
