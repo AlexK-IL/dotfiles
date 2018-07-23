2b1bfc845f86573a148d42f5e5aae63456ef4b19
Alex Kubica <alexkubicail@gmail.com>
2018-07-23 14:01:45 +0300

    update path

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,2 +1,2 @@
-PATH=~/.dotfiles/bin:~/.dotfiles-local/bin:$HOME/.node_modules/bin:${PATH}
+PATH=~/bin:~/.dotfiles/bin:~/.dotfiles-local/bin:$HOME/.node_modules/bin:${PATH}
 export npm_config_prefix=~/.node_modules
