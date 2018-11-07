7b312cdf67ea09da919557b2a173e75d60f71a4b
Alex Kubica <alexkubicail@gmail.com>
2018-11-07 13:58:26 +0200

    update

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,2 +1,2 @@
-PATH=~/bin:~/.dotfiles/bin:~/.dotfiles-local/bin:$HOME/.node_modules/bin:${PATH}
+PATH=~/bin:~/.dotfiles/bin:~/.dotfiles-local/bin:$HOME/.node_modules/bin:/opt/apache-spark/bin:${PATH}
 export npm_config_prefix=~/.node_modules
