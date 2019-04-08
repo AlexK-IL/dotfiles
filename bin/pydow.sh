7852493b8b8d1d7a1573cea61273be40e2a49283
Alex Kubica <alexkubica@pm.me>
2019-04-08 17:20:32 +0300

    update

--- bin/pydow.sh
+++ bin/pydow.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
-# A shell script to download python packages in different platforms
-#------------------------------------------------------------------
+# A shell script to download python packages for different platforms
+#-------------------------------------------------------------------
 
 [ $# -eq 0 ] && { echo "Usage: $0 package, [package, ...]"; exit 1; }
+
+declare -a py_versions=("cp27m", "cp36m", "cp37m")
+declare -a platforms=("win32", "win_amd64", "manylinux1_x86", "manylinux1_x86_64", "linux_x86", "linux_x86_64")
 
 for package in "$@"
 do
     pip download $package
-    pip download $package --platform win32 --only-binary=:all:
-    pip download $package --platform win_amd64 --only-binary=:all:
-    pip download $package --platform manylinux1_x86 --only-binary=:all:
-    pip download $package --platform manylinux1_x86_64 --only-binary=:all:
-    pip download $package --platform linux_x86 --only-binary=:all:
-    pip download $package --platform linux_x86_64 --only-binary=:all:
-    pip download $package --platform any --only-binary=:all:
+
+    for platform in "${platforms[@]}"
+    do
+        for py_version in "${py_versions[@]}"
+        do
+            # pip download $package --python-version $py_version --platform $platform --only-binary=:all:
+            pip download $package --platform $platform --only-binary=:all:
+        done
+    done
 done
